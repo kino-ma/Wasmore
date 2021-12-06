c6bbdaede82a1906c8924a06a1cec7b10dde549b
kino-ma <ma@kino.ma>
2021-12-06 16:46:17 +0900

    [feat] add index to controller

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+const indexRouter = require('./routes/index');
 const heavyRouter = require('./routes/heavy');
 const lightRouter = require('./routes/light');
 
+app.use('/', indexRouter);
 app.use('/light-task', lightRouter);
 app.use('/heavy-task', heavyRouter);
 
