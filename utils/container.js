b3d6f2762e69b635162882315f57757885838f92
kino-ma <ma@kino.ma>
2021-12-09 18:49:38 +0900

    [feat] create container

--- utils/container.js
+++ utils/container.js
@@ -8,4 +8,18 @@ const listContainers = async () => {
   return json;
 }
 
-listContainers().then(console.log)
+const createContainer = async ({ command, image, ...config }) => {
+  const data = {
+    Cmd: command,
+    Image: image,
+    ...config
+  }
+
+  const body = JSON.stringify(data)
+
+  const resp = await Request.post("/containers/create", body, { headers: { "Content-Type": "application/json" } });
+  const json = JSON.parse(resp);
+  return json;
+}
+
+createContainer({ command: ["date"], image: "ubuntu" }).then(console.log)
