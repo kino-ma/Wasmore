c0a61c4e4b53f217b9f9e8461ab00cdf92e322dd
kino-ma <ma@kino.ma>
2021-12-09 19:03:13 +0900

    [feat] create & run container

--- utils/container.js
+++ utils/container.js
@@ -3,3 +3,10 @@ const dockerSocket = "/var/run/docker.sock";
 const docker = new Docker(dockerSocket);
 
 module.exports = docker;
+
+docker.run('ubuntu', ["date"], process.stdout, {}, function (err, data, container) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.StatusCode);
+});
