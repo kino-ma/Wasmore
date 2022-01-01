49df073ff166501efdc431a341a870be8451e712
kino-ma <ma@kino.ma>
2022-01-01 02:47:33 -0500

    [feat] use `Container` version dateRunner on invoker

--- server/invoker.js
+++ server/invoker.js
@@ -16,38 +16,10 @@ const container = (input) => {
     });
 };
 
-const date = (_input) => {
-  console.log({ dateRunner });
-  const stdout = new streams.WritableStream();
-  return dateRunner
-    .then((container) => {
-      console.log({ container });
-
-      container
-        .attach({ stream: true, stdout: true, stderr: true })
-        .then((stream) => {
-          stream.pipe(stdout);
-        });
-
-      return container.start();
-    })
-    .then((res) => {
-      console.debug({ collResult: res });
-      const decoded = stdout.toString();
-      console.debug({ decoded });
-
-      const output = {
-        stdout: decoded,
-        callResult: res
-      };
-
-      return output;
-    })
-    .catch((err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+const date = async (_input) => {
+  const container = dateRunner;
+  const { stdout } = await container.run();
+  return stdout;
 };
 
 module.exports = {
--- server/invoker.test.js
+++ server/invoker.test.js
@@ -3,9 +3,6 @@ const { date } = require("./invoker")
 describe("Test Invoker", () => {
   test("It should return date", async () => {
     const output = await date();
-    const { stdout, callResult } = output;
-    console.debug({ output })
-    expect(callResult).not.toBeFalsy();
-    expect(stdout).not.toBe(undefined);
+    expect(output).not.toBeFalsy();
   });
 });
