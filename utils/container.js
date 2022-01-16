271ec65414a8038201bbf65782362fee1ac24622
kino-ma <ma@kino.ma>
2022-01-16 07:21:42 -0500

    [feat] read stdin to exec sh

--- utils/container.js
+++ utils/container.js
@@ -48,6 +48,7 @@ class Container {
       Cmd: command,
       AttachStdout: true,
       AttachStderr: true,
+      AttachStdin: true,
       Tty: true,
     };
 
@@ -58,24 +59,62 @@ class Container {
     // let after = performance.now();
     // console.log(`exec create took: ${after - before} ms`);
 
+    // const buf = new streams.ReadableStream("hello");
+
     // before = performance.now();
-    const stream = await exe.start();
+    console.log("exec start");
+    const execStartOptions = {
+      // hijack: true,
+      stdin: true,
+    }
+    const stream = await exe.start(execStartOptions);
+    console.log("exec start end");
+    console.log({ execStream: stream });
     // after = performance.now();
     // console.log(`exec start took: ${after - before} ms`);
 
-    stream.pipe(stdout);
+    // stream.on('readable', () => {
+    //   console.log("readable", stream.read());
+    // })
 
     // before = performance.now();
     const closedStream = new Promise((resolve, reject) => {
       stream.on('close', () => {
         // after = performance.now();
         // console.log(`stream close took: ${after - before} ms`);
+        console.log("close event")
+        const output = stdout.toString();
+        resolve(output)
+      })
+      stream.on('end', () => {
+        // after = performance.now();
+        // console.log(`stream close took: ${after - before} ms`);
+        console.log("end event")
         const output = stdout.toString();
         resolve(output)
       })
 
       stream.on('error', reject);
     })
+
+    // const res = stream.write("nodejs writing\r\n second line");
+    // stream.write("nodejs writing\r\n second line");
+    // stream.write("nodejs writing\r\n second line");
+    // stream.write("nodejs writing\r\n second line");
+    // stream.write("nodejs writing\r\n second line");
+    // stream.write("nodejs writing\r\n second line");
+    // console.log({ writtenStream: stream, writeResult: res });
+    // const ended = stream.end();
+    // console.log("ended writing", { ended })
+
+    process.stdin.setRawMode(true)
+    stream._output.pipe(process.stdout);
+    process.stdin.pipe(stream);
+    // stream.write("echo hello; ls /");
+    console.log("piped")
+    process.stdin.setRawMode(false)
+    process.stdin.resume()
+
 
     return closedStream;
   }
@@ -84,8 +123,7 @@ class Container {
 class CachingContainer extends Container {
   constructor(command, customOptions = {}) {
     const defaultOptions = {
-      Image: "ubuntu",
-      Cmd: ["sleep", "infinity"],
+      Image: "faas-bin",
       AttachStdout: true,
       AttachStderr: true,
       Tty: true,
@@ -129,8 +167,12 @@ const containers = {
 
 const dateRunner = new CachingContainer(["date", "+%s"]);
 
+// const helloContainer = new CachingContainer(["/root/faas_bin", "hello"]);
+// const helloContainer = new CachingContainer(["timeout", "4", "head", "-n", "1"]);
+const helloContainer = new CachingContainer(["sh"]);
+
 const callContainer = () => {
   return docker.run("ubuntu", ["date", "+%s"], process.stdout, { AutoRemove: true });
 };
 
-module.exports = { Container, CachingContainer, docker, callContainer, dateRunner };
+module.exports = { Container, CachingContainer, docker, callContainer, dateRunner, helloContainer };
