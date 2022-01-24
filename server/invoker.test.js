c5da72a10b7b6a0a1239c43a0561fb33faeb926e
kino-ma <ma@kino.ma>
2022-01-23 22:58:34 -0500

    [test] not returns wrong value

--- server/invoker.test.js
+++ server/invoker.test.js
@@ -6,9 +6,13 @@ describe("Test Invoker", () => {
     expect(output).not.toBeFalsy();
   });
 
-  test("wasm invoker", async () => {
+  test("wasm invoker return correct value", async () => {
     const name = "kino-ma";
     const output = await invokeWasmHello(name);
     expect(output).toBe(`hello, ${name}`);
+
+    const name2 = "makino";
+    const output2 = await invokeWasmHello(name2 + "dayo");
+    expect(output2).not.toBe(`hello, ${name2}`);
   })
 });
