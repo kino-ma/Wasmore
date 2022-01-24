fc70834b3b2e2b7aa4884cea00d6d056e2215b84
kino-ma <ma@kino.ma>
2022-01-23 22:44:33 -0500

    [test] wasm call integrity

--- server/invoker.test.js
+++ server/invoker.test.js
@@ -1,8 +1,14 @@
-const { date } = require("./invoker")
+const { date, hello } = require("./invoker")
 
 describe("Test Invoker", () => {
   test("Date invoker  should return date", async () => {
     const output = await date();
     expect(output).not.toBeFalsy();
   });
+
+  test("wasm invoker", async () => {
+    const name = "kino-ma";
+    const output = hello(name);
+    expect(output).toBe(`hello, ${name}`);
+  })
 });
