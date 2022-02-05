883b878d99cc7cec93feff0bcf866401d5b1b43f
kino-ma <ma@kino.ma>
2022-02-05 03:37:59 -0500

    [test] `ReusableInvoker`

--- server/invoker.test.js
+++ server/invoker.test.js
@@ -1,5 +1,5 @@
 const { VM } = require("vm2");
-const { date, invokeWasmHello } = require("./invoker")
+const { date, invokeWasmHello, ReusableInvoker } = require("./invoker");
 
 describe("Test Invoker", () => {
   test("Date invoker  should return date", async () => {
@@ -15,17 +15,53 @@ describe("Test Invoker", () => {
     const name2 = "makino";
     const output2 = await invokeWasmHello(name2 + "dayo");
     expect(output2).not.toBe(`hello, ${name2}`);
-  })
+  });
 
   test("vm2 has correct access rights", () => {
     const sandbox = {
-      hoge: "fuga"
-    }
+      hoge: "fuga",
+    };
     const vm = new VM({ sandbox });
 
     const hoge = vm.run("hoge");
     expect(hoge).toBe(sandbox.hoge);
 
-    expect(() => vm.run("fuga")).toThrow('fuga is not defined');
-  })
+    expect(() => vm.run("fuga")).toThrow("fuga is not defined");
+  });
+});
+
+describe("Test ReusableInvoker class", () => {
+  class SampleInvoker extends ReusableInvoker {
+    constructor() {
+      super();
+    }
+
+    async _invoke() {
+      return { result: "hello", elapsed: 16 };
+    }
+  }
+
+  test("Invoker can invoke task", async () => {
+    const invoker = new SampleInvoker();
+    const { result, elapsed } = await invoker._invoke();
+    expect(result).toBe("hello");
+    expect(elapsed).toBe(16);
+  });
+
+  test("Invoker can run", async () => {
+    const invoker = new SampleInvoker();
+    const result = await invoker.run();
+    expect(result).toBe("hello");
+  });
+
+  test("Invoker can be reused and get average elapsed time", async () => {
+    const invoker = new SampleInvoker();
+    for (let i = 0; i < 10; i += 1) {
+      const result = await invoker.run();
+      expect(result).toBe("hello");
+    }
+    const avgElapsed = invoker.averageElapsedTime;
+    // approximately equals to 16 with 2 significant figures
+    expect(avgElapsed).toBeCloseTo(16);
+  });
 });
