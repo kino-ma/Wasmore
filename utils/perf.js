0eb67cef5defbaa73610f8354878d375f40b30be
kino-ma <ma@kino.ma>
2022-02-05 01:35:30 -0500

    [feat] add `perf.measure`

---
+++ utils/perf.js
@@ -0,0 +1,14 @@
+const measure = async (name, fn) => {
+  const before = performance.now();
+  const result = await fn();
+  const after = performance.now();
+  const elapsed = after - before;
+  console.log(`[ELAPSED] ${name}: ${elapsed} ms`);
+
+  return {
+    result,
+    before,
+    after,
+    elapsed,
+  };
+}
