0a143a3c5cc5591d0189136d6686530f7669ff53
kino-ma <ma@kino.ma>
2022-02-05 07:08:46 -0500

    [refactor] switch input type

--- server/invoker/container.js
+++ server/invoker/container.js
@@ -1,16 +1,19 @@
 const { measure } = require("../../utils/perf");
 const { ReusableInvoker } = require("./invoker");
 
+const intTasks = ["light", "heavy"];
+
 class ContainerInvoker extends ReusableInvoker {
   constructor(cachingContainer, task) {
     super();
     this.container = cachingContainer;
     this.task = task;
+    this.isIntTask = intTasks.includes(task);
   }
 
   async _invoke(input) {
     return this.container.startAndExec({
-      input: parseInt(input),
+      input: this.isIntTask ? parseInt(input) : input,
       task: this.task,
     });
   }
