b35ac2b219abc4d4975f86f0aeac0fe7c5a715b2
kino-ma <ma@kino.ma>
2022-02-05 04:11:39 -0500

    [test] `WasmInvoker`

--- server/invoker.test.js
+++ server/invoker.test.js
@@ -1,3 +1,4 @@
+const { hello } = require("faas-app");
 const { VM } = require("vm2");
 const { helloContainer } = require("../utils/container");
 const {
@@ -5,6 +6,7 @@ const {
   invokeWasmHello,
   ReusableInvoker,
   ContainerInvoker,
+  WasmInvoker,
 } = require("./invoker");
 
 describe("Test Invoker", () => {
@@ -92,5 +94,42 @@ describe("Test ContainerInvoker", () => {
     const expectedResult = expect.stringContaining("hello");
 
     expect(result).toEqual(expectedResult);
+  });
+});
+
+describe("Test WasmInvoker", () => {
+  const name = "hoge";
+  const expectedResult = `hello, ${name}`;
+
+  test("WasmInvoker can _invoke()", async () => {
+    const func = hello;
+    const invoker = new WasmInvoker(func);
+
+    const { result, elapsed } = await invoker._invoke(name);
+
+    expect(result).toEqual(expectedResult);
+    expect(elapsed).toBeGreaterThan(0);
+  });
+
+  test("WasmInvoker can run", async () => {
+    const func = hello;
+    const invoker = new WasmInvoker(func);
+
+    const result = await invoker.run(name);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("WasmInvoker can reused many times", async () => {
+    const func = hello;
+    const invoker = new WasmInvoker(func);
+
+    for (let i = 0; i < 10; i += 1) {
+      const result = await invoker.run(name);
+      expect(result).toEqual(expectedResult);
+    }
+    const avgElapsed = invoker.averageElapsedTime;
+    // approximately equals to 16 with 2 significant figures
+    expect(avgElapsed).toBeGreaterThan(0);
   });
 });
