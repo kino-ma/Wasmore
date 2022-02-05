579a7fb7c93840bb93ac0236d8167dc683dfee7e
kino-ma <ma@kino.ma>
2022-02-05 03:18:46 -0500

    [feat] abstract class `ReusableInvoker`

--- server/invoker.js
+++ server/invoker.js
@@ -2,38 +2,83 @@ const streams = require("memory-streams");
 const { VM } = require("vm2");
 
 const { hello, heavy_task, light_task } = require("faas-app");
-const { callContainer, dateRunner, helloContainer, lightContainer, heavyContainer } = require("../utils/container");
+const {
+  callContainer,
+  dateRunner,
+  helloContainer,
+  lightContainer,
+  heavyContainer,
+} = require("../utils/container");
+
+// Abstract class/interface. Subclasses must implement `constructor()` and `_invoke()`.
+class ReusableInvoker {
+  constructor() {
+    if (this.constructor === ReusableInvoker) {
+      throw new TypeError(
+        "Abstract class ReusableInvoker cannot be instantiated directly"
+      );
+    }
+
+    this.elapsedTimeHistory = [];
+  }
+
+  // Required method to implement this abstract class
+  // Must return { elapsed, result }
+  async _invoke(_input) {
+    throw new TypeError("You must implement `async _invoke()`");
+  }
+
+  // --- Provided methods ---
+
+  async run(input) {
+    const { elapsed, result } = await this._invoke(input);
+    this.elapsedTimeHistory.push(elapsed);
+    return result;
+  }
+
+  get averageElapsedTime() {
+    const sum = this.elapsedTimeHistory.reduce(
+      ({ c, s, u }, { create, start, userProgram }) => ({
+        create: create + c,
+        start: start + s,
+        userProgram: userProgram + u,
+      })
+    );
+
+    return sum / this.elapsedTime.execs.length;
+  }
+}
 
 const callVirtualized = (func, arg) => {
   const globalObject = {
     func,
-    arg
+    arg,
   };
 
   const vm = new VM({
-    sandbox: globalObject
+    sandbox: globalObject,
   });
 
   return vm.run("func(arg)");
-}
+};
 
 const heavy = (input) => {
-  const container = heavyContainer
+  const container = heavyContainer;
   if (!container.running) {
     container.manualStart();
     return callVirtualized(heavy_task, input);
   } else {
-    return container.startAndExec({ input: parseInt(input), task: "heavy" })
+    return container.startAndExec({ input: parseInt(input), task: "heavy" });
   }
 };
 
 const light = (input) => {
-  const container = lightContainer
+  const container = lightContainer;
   if (!container.running) {
     container.manualStart();
     return callVirtualized(light_task, input);
   } else {
-    return container.startAndExec({ input: parseInt(input), task: "light" })
+    return container.startAndExec({ input: parseInt(input), task: "light" });
   }
 };
 
@@ -60,11 +105,11 @@ const date = async (_input) => {
 const invokeHello = async (input) => {
   const container = helloContainer;
   return container.startAndExec({ input, task: "hello" });
-}
+};
 
 const invokeWasmHello = async (name) => {
   return callVirtualized(hello, name);
-}
+};
 
 module.exports = {
   heavy,
@@ -72,5 +117,5 @@ module.exports = {
   container,
   date,
   invokeWasmHello,
-  invokeHello
+  invokeHello,
 };
