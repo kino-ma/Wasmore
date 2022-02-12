58352f2cd86c324c922b389789bc370931e9ab2d
kino-ma <ma@kino.ma>
2022-02-12 11:00:18 -0500

    [feat] add threads support for `WasmInvoker`

--- server/invoker/wasm.js
+++ server/invoker/wasm.js
@@ -23,6 +23,24 @@ class WasmInvoker extends ReusableInvoker {
   }
 }
 
+expose({
+  invokers: {},
+
+  addModule(name, func) {
+    const invoker = new WasmInvoker(func);
+    this.invokers[name] = invoker;
+  },
+
+  runModule(name, input) {
+    if (!name in this.invokers) {
+      throw new Error(`no such ContainerInvoker: ${name}`);
+    }
+
+    const invoker = this.invokers[name];
+    return invoker.run(input);
+  },
+});
+
 module.exports = {
   WasmInvoker,
 };
