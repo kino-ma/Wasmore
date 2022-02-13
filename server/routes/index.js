aa673bff246db18125af627f169fafeb35b5e3b8
kino-ma <ma@kino.ma>
2022-02-13 08:44:18 -0500

    [refactor] respond by plane JS
    To know base latencies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const { hello } = require("faas-app");
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  res.send(hello('node'));
+router.get("/", async function (req, res, next) {
+  res.send("hello, node");
 });
 
 module.exports = router;
