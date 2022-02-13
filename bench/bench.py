dc8f3fb0d31c90e84dc40ec4247188efd53eb68a
kino-ma <ma@kino.ma>
2022-02-13 04:34:22 -0500

    [refactor] bench 1 path 1 run

--- bench/bench.py
+++ bench/bench.py
@@ -4,24 +4,42 @@
 import sys
 import datetime
 import time
+import csv
 
 import requests
 
 REQUEST_COUNT = 10
 INTERVAL = 1
 
+
 def average(values, zero_value=datetime.timedelta(0)):
     return sum(values, zero_value) / len(values)
 
+
 def show_delta(delta):
-    return f"{delta.seconds * 1_000_000 + delta.microseconds: >8} μs"
+    return f"{format_delta(delta): >8} μs"
+
+
+def format_delta(delta):
+    return f"{delta.seconds * 1_000_000 + delta.microseconds}"
+
 
 def print_deltas(deltas):
     for d in deltas:
         print(show_delta(d))
-    print("average: ", show_delta(average(deltas)))
-    print("min:     ", show_delta(min(deltas)))
-    print("max:     ", show_delta(max(deltas)))
+    print("average: ", show_delta(average(deltas)), file=sys.stderr)
+    print("min:     ", show_delta(min(deltas)), file=sys.stderr)
+    print("max:     ", show_delta(max(deltas)), file=sys.stderr)
+
+
+def write_csv(latencies, file):
+    data = [{'count': i, 'latency': format_delta(
+        l)} for i, l in enumerate(latencies, start=1)]
+    writer = csv.DictWriter(file, fieldnames=data[0].keys())
+
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
 
 
 def send_request(url, input=None):
@@ -35,8 +53,18 @@ def send_request(url, input=None):
         print(f"request to {url}")
         return requests.get(url)
 
-def main():
+
+def main(path=None):
     base_url = "http://localhost:3000"
+
+    if not path:
+        path = "/hello"
+
+    if not path.startswith("/"):
+        path = f"/{path}"
+
+    print(file=sys.stderr)
+    print(f" === START BENCHMARK FOR {path} == ", file=sys.stderr)
 
     # calculate average latency using `/` (root)
     url = f"{base_url}/"
@@ -47,40 +75,26 @@ def main():
         latencies.append(resp.elapsed)
 
     base_latency = average(latencies)
-    print("base latency:", base_latency.microseconds)
+    print("base latency:", base_latency.microseconds, file=sys.stderr)
 
     time.sleep(1)
 
     # light tasks
-    light_latencies = []
+    latencies = []
 
-    url = f"{base_url}/light-task"
-    for i in range (REQUEST_COUNT):
+    url = f"{base_url}{path}"
+    for i in range(REQUEST_COUNT):
         time.sleep(INTERVAL)
         response = send_request(url, 92)
         elapsed = response.elapsed - base_latency
-        light_latencies.append(elapsed)
+        latencies.append(elapsed)
 
-    time.sleep(1)
+    print(f"## {path} ##", file=sys.stderr)
+    print_deltas(latencies)
 
-    # heavy tasks
-    heavy_latencies = []
-
-    url = f"{base_url}/heavy-task"
-    for i in range (REQUEST_COUNT):
-        time.sleep(INTERVAL)
-        response = send_request(url, 50_000_000)
-        elapsed = response.elapsed - base_latency
-        heavy_latencies.append(elapsed)
-
-    print()
-    print("## light ##")
-    print_deltas(light_latencies)
-
-    print()
-    print("## heavy ##")
-    print_deltas(heavy_latencies)
+    write_csv(latencies, sys.stdout)
 
 
 if __name__ == "__main__":
-    main()
+    path = sys.argv[1] if len(sys.argv) > 1 else None
+    main(path)
