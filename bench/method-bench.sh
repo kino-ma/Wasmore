e5aaca3015ae0fddb165ba3743d1155dc6964b9b
kino-ma <ma@kino.ma>
2022-02-20 17:43:46 -0500

    [refactor] bench many-light in separate loop

--- bench/method-bench.sh
+++ bench/method-bench.sh
@@ -34,6 +34,36 @@ run() {
     path="/$task-task"
     python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task.csv"
 
+    # MANY LIGHT
+    task=light
+    living_containers="$out_dir/$RUN_METHOD-$task-living-containers.csv"
+    echo "count,time,living-containers" > "$living_containers"
+    input=92
+    path="/$task-tasks/1"
+
+    echo
+    echo
+    echo "start many $task"
+    echo
+
+    for count in {1..10}
+    do
+        echo
+        echo "-- many #$count --"
+        echo
+
+        python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
+
+        time="$(date +%R:%S)"
+        # grep exists with 0 if none matched
+        containers="$(docker ps | grep -v 'CONTAINER ID' | wc -l || true)"
+        /bin/echo "number of containers: #$count ($time): $containers"
+        echo "$count,$time,$containers" >> $living_containers
+        echo
+        
+        sleep 5m
+    done
+
     # MANY HEAVY
     task=heavy
     living_containers="$out_dir/$RUN_METHOD-$task-living-containers.csv"
@@ -41,17 +71,11 @@ run() {
 
     echo
     echo
-    echo "start many"
+    echo "start many heavy"
     echo
 
     for count in {1..10}
     do
-        task=light
-        input=92
-        path="/$task-task"
-        python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
-
-        task=heavy
         input=500
 
         echo
