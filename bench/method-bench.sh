26e1fefd5ee80d74731b3de1f1dd6718ae25f7ed
kino-ma <ma@kino.ma>
2022-04-30 03:24:08 -0400

    [docs] measure light by 100 loops for all methods

--- bench/method-bench.sh
+++ bench/method-bench.sh
@@ -34,44 +34,16 @@ run() {
     # path="/$task-task"
     # python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task.csv"
 
-    # # MANY LIGHT
-    # task=light
-    # living_containers="$out_dir/$RUN_METHOD-$task-living-containers.csv"
-    # echo "count,time,living-containers" > "$living_containers"
-    # input=92
-    # path="/$task-tasks/1"
-
-    # echo
-    # echo
-    # echo "start many $task"
-    # echo
-
-    # for count in {1..100}
-    # do
-    #     echo
-    #     echo "-- many #$count --"
-    #     echo
-
-    #     python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
-
-    #     time="$(date +%R:%S)"
-    #     # grep exists with 0 if none matched
-    #     containers="$(docker ps | grep -v 'CONTAINER ID' | wc -l || true)"
-    #     /bin/echo "number of containers: #$count ($time): $containers"
-    #     echo "$count,$time,$containers" >> $living_containers
-    #     echo
-        
-    #     sleep 5m
-    # done
-
-    # MANY HEAVY
-    task=heavy
+    # MANY LIGHT
+    task=light
     living_containers="$out_dir/$RUN_METHOD-$task-living-containers.csv"
     echo "count,time,living-containers" > "$living_containers"
+    input=92
+    path="/$task-tasks/1"
 
     echo
     echo
-    echo "start many heavy"
+    echo "start many $task"
     echo
 
     for count in {1..100}
@@ -80,26 +52,54 @@ run() {
         echo "-- many #$count --"
         echo
 
-        # for i in {2..7}
-        # do
-        #     path="/$task-tasks/$i"
-        #     python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
-        #     input+=0
-        #     sleep 1
-        # done
-
-        input=50000000
-        path="/$task-tasks/1"
         python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
+
         time="$(date +%R:%S)"
         # grep exists with 0 if none matched
         containers="$(docker ps | grep -v 'CONTAINER ID' | wc -l || true)"
         /bin/echo "number of containers: #$count ($time): $containers"
         echo "$count,$time,$containers" >> $living_containers
         echo
-
+      
         sleep 5m
     done
+
+    # # MANY HEAVY
+    # task=heavy
+    # living_containers="$out_dir/$RUN_METHOD-$task-living-containers.csv"
+    # echo "count,time,living-containers" > "$living_containers"
+
+    # echo
+    # echo
+    # echo "start many heavy"
+    # echo
+
+    # for count in {1..100}
+    # do
+    #     echo
+    #     echo "-- many #$count --"
+    #     echo
+
+    #     # for i in {2..7}
+    #     # do
+    #     #     path="/$task-tasks/$i"
+    #     #     python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
+    #     #     input+=0
+    #     #     sleep 1
+    #     # done
+
+    #     input=50000000
+    #     path="/$task-tasks/1"
+    #     python3 bench.py "$path" "$input" > "$out_dir/$RUN_METHOD-$task-$input-$count.csv"
+    #     time="$(date +%R:%S)"
+    #     # grep exists with 0 if none matched
+    #     containers="$(docker ps | grep -v 'CONTAINER ID' | wc -l || true)"
+    #     /bin/echo "number of containers: #$count ($time): $containers"
+    #     echo "$count,$time,$containers" >> $living_containers
+    #     echo
+
+    #     sleep 5m
+    # done
 
     # kill all node children
     ps ax -o pid=,command | grep node | grep -vE 'vscode|grep' | awk '{print $1}' | xargs kill
